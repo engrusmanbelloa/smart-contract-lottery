c289450316c54b03c7dcb6459701caf5e8136af7
engrusmanbelloa <usmanbelloa@gmail.com>
2024-11-18 14:31:16 +0100

    first

---
+++ src/Raffle.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+/**
+ * @author Usman Bello A.
+ * @title  Raffle contract.
+ * @dev    Chainlink VRF implimentation.
+ * @notice Raffle contract
+ */
+
+contract Raffle {
+    constructor() {}
+}
